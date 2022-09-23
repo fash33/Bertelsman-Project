c9f1dfcda724a0ece484168f6c24f6e01dc68ba2
fash33 <farhan.hamid39@gmail.com>
2022-09-24 00:55:19 +0600

    docs: Made changes to predict.py

--- Project 2/predict.py
+++ Project 2/predict.py
@@ -1,6 +1,5 @@
-import argparse
-import warnings 
-import time
+import argparse, time, warnings
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,17 +10,28 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds 
-import logging
-import json
+import logging,json
 
-p = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-p.add_argument('input', action="store", type = str, help = 'Image path')
-p.add_argument('model',action="store", type = str, help = 'Classifier path')
-p.add_argument('--top_k', default = 5, action = "store", type = int, help = 'Return the top K most likely classes')
-p.add_argument('--category_names', default = './label_map.json', action = "store", type = str, help = 'JSON file mapping labels')
-arg_parser = p.parse_args()
-top_k = arg_parser.top_k
+parser.add_argument('input', action="store", type = str, help = 'Image path')
+parser.add_argument('model',action="store", type = str, help = 'Classifier path')
+parser.add_argument('--top_k', default = 5, action = "store", type = int, help = 'Return the top K most likely classes')
+parser.add_argument('--category_names', default = './label_map.json', action = "store", type = str, help = 'JSON file mapping labels')
+arg_p = parser.parse_args()
+top_k = arg_p.top_k
+
+def predict(image_path, model, top_k):
+    img = Image.open(image_path)
+    samp_image = np.asarray(img)
+    processed_samp_image = process_image(samp_image)
+    probability_predictions = model.predict(np.expand_dims(processed_samp_image, axis = 0))
+    
+    vals,indices = tf.nn.top_k(probability_predictions, k = top_k)
+    probabilities = list(vals.numpy()[0])
+    classes = list(indices.numpy()[0])
+    
+    return probabilities, classes
 
 def process_image(image):
     image = tf.convert_to_tensor(image, dtype=tf.float32)
@@ -29,30 +39,19 @@ def process_image(image):
     image /= 255
     return image
 
-def predict(image_path, model, top_k):
-    img = Image.open(image_path)
-    test_image = np.asarray(img)
-    processed_test_image = process_image(test_image)
-    prob_preds = model.predict(np.expand_dims(processed_test_image, axis = 0))
-    
-    values,indices = tf.nn.top_k(prob_preds, k = top_k)
-    probs = list(values.numpy()[0])
-    classes = list(indices.numpy()[0])
-    
-    return probs, classes
-
-with open(arg_parser.category_names, "r") as file:
+with open(arg_p.category_names, "r") as file:
     mapping = json.load(file)
 
-loaded_model = tf.keras.models.load_model(arg_parser.model, compile = False, custom_objects = {'KerasLayer':hub.KerasLayer})
+loaded_model = tf.keras.models.load_model(arg_p.model, custom_objects = {'KerasLayer':hub.KerasLayer}, compile = False)
 print(f"\n Top {top_k} Classes \n")
-probs, labels = predict(arg_parser.input, loaded_model, top_k)
+probabilities, labels = predict(arg_p.input, loaded_model, top_k)
 
-for prob, label in zip(probs, labels):
-    print('Label:', label)
-    print('Class name:', mapping[str(label+1)].title())
-    print('Probability:',prob)
+for probability, label in zip(probabilities, labels):
+    print('Image Label:', label)
+    print('Classname:', mapping[str(label+1)].title())
+    print('Probability:',probability)
                                  
 
                              
     
+
